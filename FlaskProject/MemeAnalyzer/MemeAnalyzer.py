5a9069cd91d84864030fe3dd20af625b3ce81bad
Carmina Perez Guerrero <carminita_perez@hotmail.com>
2019-05-07 17:44:14 -0500

    Add color analysis

--- FlaskProject/MemeAnalyzer/MemeAnalyzer.py
+++ FlaskProject/MemeAnalyzer/MemeAnalyzer.py
@@ -3,11 +3,72 @@ import cv2
 import matplotlib.pyplot as plt
 import numpy as np
 import io
-
+from sklearn.cluster import KMeans
 from uuid import uuid4
 from flask import Flask, request, render_template, send_from_directory
 
 from prediction import runPrediction
+
+def find_histogram(clt):
+    numLabels = np.arange(0, len(np.unique(clt.labels_)) + 1)
+    (hist, _) = np.histogram(clt.labels_, bins=numLabels)
+
+    hist = hist.astype("float")
+    hist /= hist.sum()
+
+    return hist
+
+def plot_colors2(hist, centroids):
+    bar = np.zeros((50, 300, 3), dtype="uint8")
+    startX = 0
+
+    for (percent, color) in zip(hist, centroids):
+        # plot the relative percentage of each cluster
+        endX = startX + (percent * 300)
+        cv2.rectangle(bar, (int(startX), 0), (int(endX), 50),
+                      color.astype("uint8").tolist(), -1)
+        startX = endX
+
+    # return the bar chart
+    return bar
+
+def rgb2hsv(colorarray_rgb):
+    H = np.zeros(2)
+    S = np.zeros(2)
+    V = np.zeros(2)
+    
+    Percent  = np.zeros(2)
+    
+    for colors in np.arange(0,2):
+        R = colorarray_rgb[colors][0]/255
+        G = colorarray_rgb[colors][1]/255
+        B = colorarray_rgb[colors][2]/255
+        
+        Percent[colors] = 100*hist[colors]/hist.sum()
+        
+        Max = max(R, G, B)
+        Min = min(R, G, B)
+
+        if Max == Min:
+            H[colors] = 0
+        elif Max == R:
+            H[colors] = 60*((G-B)/(Max-Min))
+        elif Max == G:
+            H[colors] = 60*(2 + (B-R)/(Max-Min))
+        elif Max == B:
+            H[colors] = 60*(4 + (R-G)/(Max-Min))
+
+        if H[colors] < 0:
+            H[colors] += 360
+
+        if Max == 0:
+            S[colors] = 0
+        else:
+            S[colors] = 100*(Max-Min)/Max
+
+        V[colors] = Max*100
+        
+    return zip(Percent, H, S, V)
 
 app = Flask(__name__)
 # app = Flask(__name__, static_folder="images")
@@ -45,14 +106,21 @@ def upload():
     ##### PREDICTION
     category, pred = runPrediction(img)
     print(category)
-    ##### EXAMPLE RGB ANALYSIS
-    color = ('b','g','r')
-    for i,col in enumerate(color):
-        histr = cv2.calcHist([img],[i],None,[256],[0,256])
-        plt.plot(histr,color = col)
-        plt.xlim([0,256])
+    
+    ##### COLOR PROCESSING
+    imgCol = np.array(img, dtype = np.uint8)
+    imgCol = cv2.cvtColor(imgCol, cv2.COLOR_BGR2RGB)
 
-    plotName = "RGB.png"
+    imgCol = imgCol.reshape((imgCol.shape[0] * img.shape[1], 3)) #represent as row*column, channel number
+    clt = KMeans(n_clusters = 3) #cluster number
+    clt.fit(imgCol)
+
+    hist = find_histogram(clt)
+    bar = plot_colors2(hist, clt.cluster_centers_)
+
+    plt.axis("off")
+    plt.imshow(bar)
+    plotName = "Colors.png"
     plt.savefig("/".join([plotTarget, plotName]))
 
     ######## LDA ANALYSIS (needs tensor flow output for <result>)
